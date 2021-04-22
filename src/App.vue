ea31ddbac456f4eab89caa811aba76b5a11c29dc
Red02Raccoon <red02raccoon@gmail.com>
2021-04-22 21:36:32 +0300

    change language

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
       <section>
         <div class="settings">
           <div class="filter">
-            <div><b>Фильтр:</b> <input class="mx-3" v-model="filter" /></div>
+            <div><b>Filter:</b> <input class="mx-3" v-model="filter" /></div>
             <hr class="w-full border-t border-gray-600 my-4" />
           </div>
         </div>
@@ -40,7 +40,7 @@
               <label
                 for="wallet"
                 class="block text-sm font-medium text-gray-700 font-semibold"
-                >Тикер {{ ticker }}</label
+                >Ticker {{ ticker }}</label
               >
               <div class="mt-1 relative rounded-md shadow-md">
                 <input
@@ -70,7 +70,7 @@
               </div>
 
               <div v-if="error" class="text-sm text-red-600">
-                Такой тикер уже добавлен
+                Such ticker has been added
               </div>
             </div>
           </div>
@@ -95,7 +95,7 @@
                 d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
               ></path>
             </svg>
-            Добавить
+            Add ticker
           </button>
         </div>
         <div>You added: {{ tickers.length }} ticker(s)</div>
@@ -138,7 +138,7 @@
                   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                   clip-rule="evenodd"
                 ></path></svg
-              >Удалить
+              >Delete
             </button>
           </div>
         </dl>
@@ -156,7 +156,7 @@
               @click="page = page - 1"
               :disabled="!hasPrevPage"
             >
-              Назад
+              Prev
             </button>
             <div class="mx-4">
               <b>{{ page }}</b>
@@ -169,7 +169,7 @@
               :disabled="!hasNextPage"
               @click="page = page + 1"
             >
-              Вперед
+              Next
             </button>
           </div>
         </div>
