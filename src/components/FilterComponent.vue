3cb4c3152f441ccde6d609d86473cc4225a36e05
Red02Raccoon <red02raccoon@gmail.com>
2021-05-05 19:58:06 +0300

    add component -> FilterComponent

---
+++ src/components/FilterComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="filter flex">
+    <b>Filter:</b>
+
+    <input
+      class="mx-3"
+      :value="value"
+      @input="$emit('input-change', $event.target.value)"
+    />
+
+    <button v-if="value" @click="$emit('input-change', '')">
+      <delete-icon />
+    </button>
+  </div>
+</template>
+
+<script>
+import DeleteIcon from "./Icons/DeleteIcon.vue";
+export default {
+  components: {
+    DeleteIcon,
+  },
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: {
+    "input-change": null,
+  },
+};
+</script>
