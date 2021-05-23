9c6c4a4eb875a92f6e290a14d77ee5532cd7cd87
Red02Raccoon <red02raccoon@gmail.com>
2021-05-23 14:22:46 +0300

    create logo-component

---
+++ src/components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-link to="/" class="flex items-center">
+    <img
+      class="h-8 w-auto sm:h-10 mr-2"
+      src="https://i.pinimg.com/originals/8e/71/3c/8e713ce65aacea777d8065a5c80f6307.png"
+      alt="raccoon-logo"
+    />
+    <slot></slot>
+  </router-link>
+</template>
