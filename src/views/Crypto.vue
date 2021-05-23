36cc18a60b09dbaa17f9a5399b9a44f2b2526913
Red02Raccoon <red02raccoon@gmail.com>
2021-05-23 14:21:52 +0300

    create pages

---
+++ src/views/Crypto.vue
@@ -0,0 +1,11 @@
+<template>
+  <crypto-app></crypto-app>
+</template>
+
+<script>
+import CryptoApp from "@/containers/Crypto/Crypto.vue";
+
+export default {
+  components: { CryptoApp },
+};
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,10 @@
+<template>
+  <test-app></test-app>
+</template>
+<script>
+import TestApp from "@/containers/Test/Test.vue";
+
+export default {
+  components: { TestApp },
+};
+</script>
